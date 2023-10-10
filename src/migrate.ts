b7e434809741265dc8c405651a9d3a98a9cdaa6b
kiliman <kiliman@gmail.com>
2023-10-10 15:57:57 -0400

    🔨 Rewrite migration script
    Fix #32
    Fix #77

--- src/migrate.ts
+++ src/migrate.ts
@@ -8,84 +8,90 @@ export type RoutingConvention = 'flat-files' | 'flat-folders'
 export type MigrateOptions = {
   convention: RoutingConvention
 }
+
+const pathSepRegex = new RegExp(`\\${path.sep}`, 'g')
+const routeExtensions = ['.js', '.jsx', '.ts', '.tsx', '.md', '.mdx']
+
 export function migrate(
   sourceDir: string,
   targetDir: string,
   options: MigrateOptions = { convention: 'flat-files' },
 ) {
-  // var visitor = options.convention === 'flat-files' ? flatFiles : flatFolders
+  if (sourceDir.startsWith('./')) {
+    sourceDir = sourceDir.substring(2)
+  }
+  if (targetDir.startsWith('./')) {
+    targetDir = targetDir.substring(2)
+  }
 
-  // visitFiles(sourceDir, visitor(sourceDir, targetDir))
+  console.log(
+    `🛠️ Migrating to flat-routes using ${options.convention} convention...`,
+  )
+  console.log(`🗂️ source: ${sourceDir}`)
+  console.log(`🗂️ target: ${targetDir}`)
+  console.log()
 
   const routes = createRoutesFromFolders(defineRoutes, {
-    appDirectory: './app',
+    appDirectory: './',
     routesDirectory: sourceDir,
   })
 
-  Object.entries(routes).forEach(([_key, route]) => {
-    let file = route.file
-    let flat = file
-      .replace(/[\/]/g, '.')
-      .replace(/\._([^_.])/g, '.[_]$1')
-      .replace(/\.__/g, '._')
-      .replace(/\.index.tsx$/, '._index.tsx')
-      .replace(/^routes\./, 'routes/')
-    console.log(flat)
-  })
-}
-
-export function flatFiles(sourceDir: string, targetDir: string) {
-  return (file: string) => {
-    console.log(file)
+  Object.entries(routes).forEach(([id, route]) => {
+    let { path: routePath, file, parentId } = route
     let extension = path.extname(file)
-    let name = file.substring(0, file.length - extension.length)
-    const route = convertToRoute(name)
-    const targetFile = path.join(targetDir, `${route}${extension}`)
-    fs.cpSync(path.join(sourceDir, file), targetFile, { force: true })
-  }
-}
 
-const routeExtensions = ['.js', '.jsx', '.ts', '.tsx', '.md', '.mdx']
-export function flatFolders(sourceDir: string, targetDir: string) {
-  return (file: string) => {
-    console.log(file)
-    const extension = path.extname(file)
-    const name = file.substring(0, file.length - extension.length)
-    const route = convertToRoute(name)
-    const targetFolder = path.join(targetDir, route)
-    if (!routeExtensions.includes(extension)) {
-      return
+    let flat = convertToRoute(id, parentId!, routePath!, !!route.index)
+
+    // replace sourceDir with targetDir
+    if (file.startsWith(sourceDir)) {
+      flat = path.join(targetDir, flat.substring(sourceDir.length + 1))
     }
-    fs.mkdirSync(targetFolder, { recursive: true })
-    fs.cpSync(
-      path.join(sourceDir, file),
-      path.join(targetFolder, `/_index${extension}`),
-      {
+
+    console.log(`📝 ${id}`)
+    if (options.convention === 'flat-folders') {
+      if (!routeExtensions.includes(extension)) {
+        return
+      }
+      fs.mkdirSync(flat, { recursive: true })
+      fs.cpSync(file, path.join(flat, `/route${extension}`), {
         force: true,
-      },
-    )
+      })
+    } else if (options.convention === 'flat-files') {
+      const targetFile = `${flat}${extension}`
+      fs.cpSync(file, targetFile, { force: true })
+    }
+  })
+  console.log('🏁 Finished!')
+}
+
+export function convertToRoute(
+  id: string,
+  parentId: string,
+  routePath: string,
+  index: boolean,
+) {
+  if (parentId && parentId !== 'root') {
+    if (routePath?.includes('/')) {
+      // multi-segment route, so need to fixup parent for flat-routes (trailing _)
+      // strip parent route from route
+      let currentPath = id.substring(parentId.length + 1)
+      const [first, ...rest] = getRouteSegments(currentPath, index ?? false)
+      // rewrite id to use trailing _ for parent
+      id = `${parentId}/${first}_/${rest.join('/')}`
+    }
   }
-}
 
-export function convertToRoute(name: string) {
-  const pathSegments = name.split(path.sep)
-  return pathSegments
-    .map(pathSegment => {
-      const index = /(^|[.])index$/.test(pathSegment)
-      const routeSegments = getRouteSegments(pathSegment, index)
-      return getFlatRoute(routeSegments)
-    })
-    .join('.')
-}
+  // convert to flat route convention
+  let flat = id
+    // convert path separators to dots
+    .replace(pathSepRegex, '.')
+    // convert single _ to [_] due to conflict with new pathless layout prefix
+    .replace(/\._([^_.])/g, '.[_]$1')
+    // convert double __ to single _ for pathless layout prefix
+    .replace(/\.__/g, '._')
+    // convert index to _index for index routes
+    .replace(/(^|\.)index$/, '$1_index')
+  // replace sourceDir with targetDir
 
-function getFlatRoute(segments: string[]) {
-  return segments
-    .map(segment =>
-      segment.startsWith('__')
-        ? segment.substring(1)
-        : segment === 'index'
-        ? '_index'
-        : segment,
-    )
-    .join('.')
+  return flat
 }
