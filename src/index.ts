bd2e08da3c8a17aec05dfb1f81e488f2f3b64920
Steven <intsanerarity@gmail.com>
2024-12-04 08:38:38 +0100

    minimatch

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs'
-import { minimatch } from 'minimatch'
+import minimatch from 'minimatch'
 import * as path from 'path'
 import { DefineRouteFunction, RouteManifest } from './routes'
 
--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,4 @@
-import { minimatch } from 'minimatch'
+import minimatch from 'minimatch'
 import fs from 'node:fs'
 import path from 'node:path'
 import {
