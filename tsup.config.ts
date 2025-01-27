0111591e4d3d4920fec63e1c6eda386ccf34193d
Mikk Pokk <mikk@internationaleservices.eu>
2025-01-27 23:48:28 +0200

    clean directory `./dist`

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-  entry: ['src/*'],
-  format: ['cjs', 'esm'], // Build for commonJS and ESmodules
-  dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
-  sourcemap: false,
-  clean: true,
+export default defineConfig(() => {
+  const commonOptions = {
+    splitting: false,
+    sourcemap: false,
+    clean: true,
+  }
+
+  return [{
+    ...commonOptions,
+    entry: ['src/index.ts'],
+    format: ['cjs', 'esm'], // TODO: consider removing cjs support. Vite expects ESM anyway and v1 remix compiler can use serverDependenciesToBundle option
+    dts: true, // Generate declaration file (.d.ts)
+  }, {
+    ...commonOptions,
+    entry: ['src/cli.ts'],
+    format: 'cjs',
+  }]
 })
